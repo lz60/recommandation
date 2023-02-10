0aaa05aeaf6c4f22e61035d053acd84c0289dcf8
lz60 <119508887+lz60@users.noreply.github.com>
2023-02-10 19:08:07 +0100

    Add files via upload

---
+++ reommandation_articles.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 21:10:28 2023
+
+@author: zemrak
+"""
+
+
+import streamlit as st
+import pandas as pd
+
+import requests
+import numpy as np
+import  json
+
+
+st.write("""
+# application de recommandation d'articles'
+""")
+
+	
+id = st.slider("Choisir un id  UTILISATEUR: ", 0,900)
+#id=0
+
+
+  
+# Set the URL of the Azure function
+azure_url = "https://sys-recommandation.azurewebsites.net/api/recommandation_articles"
+# for example : azure_url = "https://netflix_recommendations.azurewebsites.net/api/get_recommandations/"
+
+# Set the parameters of the request
+request_params = {"user_id":id}
+
+# Send the request to the Azure function
+r = requests.post(azure_url, params=request_params)
+    
+    
+
+# Grab the recommendations as a Python dictionary
+recommendations_dict = json.loads(r.content.decode())
+st.write("les articles recommandés pour cette utilisateur sont :", recommendations_dict )
+print(recommendations_dict)
